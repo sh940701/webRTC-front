df61c951805966097c907e18efbe2524ef33daa4
sh940701 <visioner2168@gmail.com>
2024-06-13 07:32:56 +0900

    update: address

--- src/Context.js
+++ src/Context.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:8080");
+const socket = io("http://43.202.43.189:8080");
 
 const ContextProvider = ({ children }) => {
     const [callAccepted, setCallAccepted] = useState(false);
