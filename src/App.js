26f5e57537f7ec682ab03ecc67e2d792ffb2fb2f
sh940701 <visioner2168@gmail.com>
2024-06-13 07:31:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
-import VideoCall from "./VideoCall.tsx";
+import { Box, Heading, Container } from '@chakra-ui/react';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+import VideoPlayer from './components/VideoPlayer';
 
 function App() {
-    return <VideoCall/>
+    return (
+        <Box>
+            <Container maxW="1200px" mt="8">
+                <Heading as="h2" size="2xl"> Video Chat App </Heading>
+                <VideoPlayer />
+                <Options />
+                <Notifications />
+            </Container>
+        </Box>
+    );
 }
-
 export default App;
